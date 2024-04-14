f755f14df1cc230ae8c2da5f2b8549353af46ec7
MatiasPeusco <matupeusco@gmail.com>
2024-04-13 23:23:50 -0300

    Add Pagination component

---
+++ client/src/components/Pagination/Pagination.jsx
@@ -0,0 +1,48 @@
+import {
+    PaginationContainer,
+    PaginationItem,
+    PaginationButton,
+} from "./StyledPagination";
+
+const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+    const goToPage = (page) => {
+        if (page >= 1 && page <= totalPages) {
+            onPageChange(page);
+        }
+    };
+
+    return (
+        <nav>
+            <PaginationContainer>
+                <PaginationItem className="page-item">
+                    <PaginationButton
+                        onClick={() => goToPage(currentPage - 1)}
+                        disabled={currentPage === 1}
+                    >
+                        Anterior
+                    </PaginationButton>
+                </PaginationItem>
+                {Array.from({ length: totalPages }).map((_, index) => (
+                    <PaginationItem key={index} className={`page-item`}>
+                        <PaginationButton
+                            onClick={() => goToPage(index + 1)}
+                            className={` ${currentPage === index + 1 ? "active" : ""}`}
+                        >
+                            {index + 1}
+                        </PaginationButton>
+                    </PaginationItem>
+                ))}
+                <PaginationItem className="page-item">
+                    <PaginationButton
+                        onClick={() => goToPage(currentPage + 1)}
+                        disabled={currentPage === totalPages}
+                    >
+                        Siguiente
+                    </PaginationButton>
+                </PaginationItem>
+            </PaginationContainer>
+        </nav>
+    );
+};
+
+export default Pagination;
---
+++ client/src/components/Pagination/StyledPagination.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const PaginationContainer = styled.ul`
+  gap: 5px;
+  flex-wrap: wrap;
+  list-style: none;
+  padding: 0;
+  margin-top: 20px;
+  display: flex;
+  justify-content: center;
+`;
+
+export const PaginationItem = styled.li`
+  margin: 0 2px;
+`;
+
+export const PaginationButton = styled.button`
+  padding: 5px 10px;
+  border: 1px solid #6e47db;
+  border-style: outset;
+  border-radius: 6px;
+  background: #fff;
+  color: #373435;
+  text-decoration: none;
+  cursor: pointer;
+  &.active {
+    background-color: #6e47db;
+    color: #fff;
+    border-color: #007bff;
+  }
+  &:hover {
+    background-color: #ff0;
+    color: #000;
+  }
+  &:disabled {
+    cursor: not-allowed;
+    opacity: 0.6;
+  }
+  @media screen and (max-width: 768px) {
+    padding: 3px 8px;
+  }
+`;
