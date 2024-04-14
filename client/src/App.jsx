327794fc04c7b6567f59283007ec436291a06314
MatiasPeusco <matupeusco@gmail.com>
2024-04-13 21:55:10 -0300

    Added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <main>
-
-    </main>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>LANDING</div>} />
+        <Route path="/home" element={<div>HOME</div>} />
+        <Route path="/about" element={<div>ABOUUT</div>} />
+        <Route path="/countries/:id" element={<div>DETALLE</div>} />
+        <Route path="/create" element={<div>CREAR</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
