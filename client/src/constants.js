e8e62a0033166bbafe4f0ca05b332061ebc4a145
MatiasPeusco <matupeusco@gmail.com>
2024-04-14 15:17:10 -0300

    Relocate the definitions of the action constants

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,3 +1,8 @@
+export const GET_COUNTRIES = "GET_COUNTRIES";
+export const SEARCH_COUNTRIES = "SEARCH_COUNTRIES";
+export const SORT_COUNTRIES = "SORT_COUNTRIES";
+export const FILTER_COUNTRIES = "FILTER_COUNTRIES";
+
 export const CONTINENTS = [
     { name: "África", value: "Africa" },
     { name: "Sudamérica", value: "South America" },
