f16109fb5cc00db7ea902ac50cdc611e98f66d64
MatiasPeusco <matupeusco@gmail.com>
2024-04-15 17:23:24 -0300

    API: Update Country model

--- server/src/models/Country.js
+++ server/src/models/Country.js
@@ -2,11 +2,36 @@ const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo
   sequelize.define('Country', {
+    id: {
+      type: DataTypes.STRING(3), // Código de tres letras
+      primaryKey: true, // Define la clave primaria
+      allowNull: false,
+    },
     name: {
       type: DataTypes.STRING,
+      allowNull: false, // Nombre
+    },
+    flagImage: {
+      type: DataTypes.STRING, // Imagen de la bandera
       allowNull: false,
+    },
+    continent: {
+      type: DataTypes.STRING,
+      allowNull: false, // Continente
+    },
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false, // Capital
+    },
+    subregion: {
+      type: DataTypes.STRING, // Subregión
+    },
+    area: {
+      type: DataTypes.FLOAT, // Área
+    },
+    population: {
+      type: DataTypes.INTEGER, // Población
     },
   });
 };
