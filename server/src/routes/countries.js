88140122ac8271a6970a9200727b49d58db0e654
MatiasPeusco <matupeusco@gmail.com>
2024-04-16 09:40:59 -0300

    API: Add routes to filter countries by ID API

--- server/src/routes/countries.js
+++ server/src/routes/countries.js
@@ -7,8 +7,28 @@ const router = Router();
 
 router.get('/', async (req, res, next) => {
     try {
-        const countries = await Country.findAll();
-        res.status(200).json(countries);
+        const { name } = req.query; // Obtener el valor del parámetro de consulta 'name'
+
+        if (name) {
+            // Si se proporciona el parámetro de consulta 'name', buscar países por nombre
+            const countries = await Country.findAll({
+                where: {
+                    name: {
+                        [Op.iLike]: `%${name}%` // Utilizar Op.iLike para buscar de manera insensible a mayúsculas y minúsculas
+                    }
+                }
+            });
+
+            if (countries.length === 0) {
+                return res.status(404).json({ message: 'No se encontraron países con ese nombre.' });
+            }
+
+            return res.status(200).json(countries);
+        } else {
+            // Si no se proporciona el parámetro de consulta 'name', obtener todos los países
+            const countries = await Country.findAll();
+            return res.status(200).json(countries);
+        }
     } catch (error) {
         next(error);
     }
