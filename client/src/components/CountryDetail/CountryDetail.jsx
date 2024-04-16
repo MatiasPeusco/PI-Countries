8676c5d594360ed2af511f3491d635da4cb40076
MatiasPeusco <matupeusco@gmail.com>
2024-04-16 10:05:14 -0300

    Make the request for country by id

--- client/src/components/CountryDetail/CountryDetail.jsx
+++ client/src/components/CountryDetail/CountryDetail.jsx
@@ -13,187 +13,29 @@ import {
 } from "./StyledCountryDetail";
 import StyledLine from "../StyledLine";
 import Footer from "../Footer/Footer";
-
-const pais = {
-    name: {
-        common: "Argentina",
-        official: "Argentine Republic",
-        nativeName: {
-            grn: {
-                official: "Argentine Republic",
-                common: "Argentina",
-            },
-            spa: {
-                official: "República Argentina",
-                common: "Argentina",
-            },
-        },
-    },
-    tld: [".ar"],
-    cca2: "AR",
-    ccn3: "032",
-    cca3: "ARG",
-    cioc: "ARG",
-    independent: true,
-    status: "officially-assigned",
-    unMember: true,
-    currencies: {
-        ARS: {
-            name: "Argentine peso",
-            symbol: "$",
-        },
-    },
-    idd: {
-        root: "+5",
-        suffixes: ["4"],
-    },
-    capital: ["Buenos Aires"],
-    altSpellings: ["AR", "Argentine Republic", "República Argentina"],
-    region: "Americas",
-    subregion: "South America",
-    languages: {
-        grn: "Guaraní",
-        spa: "Spanish",
-    },
-    translations: {
-        ara: {
-            official: "جمهورية الأرجنتين",
-            common: "الأرجنتين",
-        },
-        ces: {
-            official: "Argentinská republika",
-            common: "Argentina",
-        },
-        cym: {
-            official: "Gweriniaeth yr Ariannin",
-            common: "Ariannin",
-        },
-        deu: {
-            official: "Argentinische Republik",
-            common: "Argentinien",
-        },
-        est: {
-            official: "Argentina Vabariik",
-            common: "Argentina",
-        },
-        fin: {
-            official: "Argentiinan tasavalta",
-            common: "Argentiina",
-        },
-        fra: {
-            official: "République argentine",
-            common: "Argentine",
-        },
-        hrv: {
-            official: "Argentinski Republika",
-            common: "Argentina",
-        },
-        hun: {
-            official: "Argentin Köztársaság",
-            common: "Argentína",
-        },
-        ita: {
-            official: "Repubblica Argentina",
-            common: "Argentina",
-        },
-        jpn: {
-            official: "アルゼンチン共和国",
-            common: "アルゼンチン",
-        },
-        kor: {
-            official: "아르헨티나 공화국",
-            common: "아르헨티나",
-        },
-        nld: {
-            official: "Argentijnse Republiek",
-            common: "Argentinië",
-        },
-        per: {
-            official: "جمهوری آرژانتین",
-            common: "آرژانتین",
-        },
-        pol: {
-            official: "Republika Argentyńska",
-            common: "Argentyna",
-        },
-        por: {
-            official: "República Argentina",
-            common: "Argentina",
-        },
-        rus: {
-            official: "Аргентинская Республика",
-            common: "Аргентина",
-        },
-        slk: {
-            official: "Argentínska republika",
-            common: "Argentína",
-        },
-        spa: {
-            official: "República Argentina",
-            common: "Argentina",
-        },
-        swe: {
-            official: "Republiken Argentina",
-            common: "Argentina",
-        },
-        urd: {
-            official: "جمہوریہ ارجنٹائن",
-            common: "ارجنٹائن",
-        },
-        zho: {
-            official: "阿根廷共和国",
-            common: "阿根廷",
-        },
-    },
-    latlng: [-34, -64],
-    landlocked: false,
-    borders: ["BOL", "BRA", "CHL", "PRY", "URY"],
-    area: 2780400,
-    demonyms: {
-        eng: {
-            f: "Argentine",
-            m: "Argentine",
-        },
-        fra: {
-            f: "Argentine",
-            m: "Argentin",
-        },
-    },
-    flag: "🇦🇷",
-    maps: {
-        googleMaps: "https://goo.gl/maps/Z9DXNxhf2o93kvyc6",
-        openStreetMaps: "https://www.openstreetmap.org/relation/286393",
-    },
-    population: 45376763,
-    gini: {
-        2019: 42.9,
-    },
-    fifa: "ARG",
-    car: {
-        signs: ["RA"],
-        side: "right",
-    },
-    timezones: ["UTC-03:00"],
-    continents: ["South America"],
-    flags: {
-        png: "https://flagcdn.com/w320/ar.png",
-        svg: "https://flagcdn.com/ar.svg",
-    },
-    coatOfArms: {
-        png: "https://mainfacts.com/media/images/coats_of_arms/ar.png",
-        svg: "https://mainfacts.com/media/images/coats_of_arms/ar.svg",
-    },
-    startOfWeek: "monday",
-    capitalInfo: {
-        latlng: [-34.58, -58.67],
-    },
-    postalCode: {
-        format: "@####@@@",
-        regex: "^([A-Z]\\d{4}[A-Z]{3})$",
-    },
-};
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 const CountryDetail = () => {
+    const { id } = useParams();
+    const [countryData, setCountryData] = useState(null);
+
+    useEffect(() => {
+        const fetchCountryData = async () => {
+            try {
+                const response = await axios.get(
+                    `http://localhost:3001/countries/${id}`
+                );
+                setCountryData(response.data);
+            } catch (error) {
+                console.error("Error al realizar la solicitud:", error);
+            }
+        };
+
+        fetchCountryData();
+    }, [id]);
+
     return (
         <>
             <Navbar />
@@ -202,26 +44,35 @@ const CountryDetail = () => {
             <Section>
                 <DetailsContainer>
                     <div>
-                        <ID>{pais.cca3}</ID>
-                        <Name>{pais.name.official}</Name>
+                        <ID>{countryData?.id}</ID>
+                        <Name>{countryData?.name}</Name>
                     </div>
                     <DetailsWrapper>
-                        <DetailInfo itemName={"Continente:"} detail={pais.continents[0]} />
-                        <DetailInfo itemName={"Capital:"} detail={pais.capital} />
-                        {pais.subregion && (
-                            <DetailInfo itemName={"Subregión:"} detail={pais.subregion} />
+                        <DetailInfo
+                            itemName={"Continente:"}
+                            detail={countryData?.continent}
+                        />
+                        <DetailInfo itemName={"Capital:"} detail={countryData?.capital} />
+                        {countryData?.subregion && (
+                            <DetailInfo
+                                itemName={"Subregión:"}
+                                detail={countryData?.subregion}
+                            />
                         )}
-                        {pais.area && (
-                            <DetailInfo itemName={"Área:"} detail={formatArea(pais.area)} />
+                        {countryData?.area && (
+                            <DetailInfo
+                                itemName={"Área:"}
+                                detail={formatArea(countryData?.area)}
+                            />
                         )}
                         <DetailInfo
                             itemName={"Población:"}
-                            detail={formatPopulation(pais.population)}
+                            detail={formatPopulation(countryData?.population)}
                         />
                     </DetailsWrapper>
                 </DetailsContainer>
                 <ImageContainer>
-                    <Image src={pais.flags.png} />
+                    <Image src={countryData?.flagImage} />
                 </ImageContainer>
             </Section>
             <Footer />
