f4cace7222b35311e86d56a4be2bb38f6e330857
MatiasPeusco <matupeusco@gmail.com>
2024-04-16 09:51:13 -0300

    API: Add route to create a new activity

--- server/src/routes/activities.js
+++ server/src/routes/activities.js
@@ -2,9 +2,7 @@ const { Router } = require("express");
 const { Op } = require("sequelize");
 const { Activity } = require("../db");
 const errorHandler = require("../middlewares/errorHandler");
-
 const router = Router();
-
 router.get('/', async (req, res, next) => {
     try {
         const activities = await Activity.findAll();
@@ -14,4 +12,29 @@ router.get('/', async (req, res, next) => {
     }
 });
 
+router.post('/', async (req, res, next) => {
+    try {
+        const { name, difficulty, duration, season, countries } = req.body;
+
+        // Crea la actividad en la base de datos
+        const activity = await Activity.create({
+            name,
+            difficulty,
+            duration,
+            season,
+        });
+
+        // Asocia la actividad con los países proporcionados
+        if (countries && countries.length > 0) {
+            await activity.setCountries(countries); // Utiliza el método setCountries proporcionado por Sequelize
+        }
+
+        res.status(201).json(activity); // Devuelve la actividad creada con éxito
+    } catch (error) {
+        next(error);
+    }
+});
+
 module.exports = router;
+
+
